f949d70fcde3b048ec7862528f64f87439fd114b
nigusmach <nigusmachin@gmail.com>
2022-07-03 20:17:25 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,8 +21,8 @@ int main(void)
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
-		if (num1 == 98 && num2 == 99)
-			continue;
+		        if (num1 == 98 && num2 == 99)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
