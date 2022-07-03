cf233afbdb94d2a3cc4ceb8bb144080b216a365c
nigusmach <nigusmachin@gmail.com>
2022-07-03 17:32:09 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,10 +3,10 @@
  * Auth: Nigus Machin
  */
 #include <stdio.h>
- /**
-  * main - Prints all single digit numbers of base 10 starting from 0.
-  * Return: Always 0.
-  */
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num;
