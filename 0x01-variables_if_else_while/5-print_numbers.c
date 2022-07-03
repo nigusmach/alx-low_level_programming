de9b8b810f5ef1aa50cff7c6d880e8e10da347db
nigusmach <nigusmachin@gmail.com>
2022-07-03 17:24:08 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,41 +1,18 @@
-uth: Brennan D Baraban
-
+/*
+ * File: 5-print_numbers.c
+ * Auth: Nigus Machin
  */
-
-
-
 #include <stdio.h>
-
-
-
  /**
-  *
-  *  * main - Prints all single digit numbers of base 10 starting from 0.
-  *
-  *   *
-  *
-  *    * Return: Always 0.
-  *
-  *     */
-
+  * main - Prints all single digit numbers of base 10 starting from 0.
+  * Return: Always 0.
+  */
  int main(void)
-
 {
+	int num;
 
-		int num;
-
-
-
-			for (num = 0; num < 10; num++)
-
-						printf("%d", num);
-
-
-
-				printf("\n");
-
-
-
-					return (0);
-
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
 }
