9b5aa7740d1393a6749b26e4123d3700a4fe5586
nigusmach <nigusmachin@gmail.com>
2022-07-03 17:51:34 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,47 +1,19 @@
 /*
- *
- *  * File: 6-print_numberz.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
-
+ * File: 6-print_numberz.c
+ * Auth: Nigus Machin
+ */
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Prints all single digit numbers of base 10 starting from 0,
- *
- *   *        only using putchar and without char variables.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ * only using putchar and without char variables.
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	int num;
 
-		int num;
-
-
-
-			for (num = 0; num < 10; num++)
-
-						putchar((num % 10) + '0');
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
 }
