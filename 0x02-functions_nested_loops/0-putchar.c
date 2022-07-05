973f57cbac8eca98532bdfa6ecef67fd5159aa81
nigusmach <nigusmachin@gmail.com>
2022-07-05 20:26:02 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,17 @@
+#include "main.h"
 /**
- *
- *  * main - Prints Holberton
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - Prints Holberton
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		_putchar('H');
-
-			_putchar('o');
-
-				_putchar('l');
-
-					_putchar('b');
-
-						_putchar('e');
-
-							_putchar('r');
-
-								_putchar('t');
-
-									_putchar('o');
-
-										_putchar('n');
-
-											_putchar('\n');
-
-												return (0);
-
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
