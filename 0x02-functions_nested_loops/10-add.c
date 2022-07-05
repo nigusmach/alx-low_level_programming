071ec68fc357b409458d07aa9e593ba05a054013
nigusmach <nigusmachin.com>
2022-07-05 12:19:45 -0700

    commited

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+/**
+ *
+ *  * add - Adds two integers and returns the result
+ *
+ *   *@x: number being added
+ *
+ *    *@y: number being added
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+
+
+int add(int x, int y)
+
+{
+
+		return (x + y);
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,31 @@
+/**
+ *
+ *  *_islower - checks for a lowercase character
+ *
+ *   *@c: letter being tested
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+
+
+int _islower(int c)
+
+{
+
+
+
+		if (c >= 97 && c <= 122)
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
