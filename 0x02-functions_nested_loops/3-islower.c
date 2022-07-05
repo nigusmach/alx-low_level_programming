ef8c047714245db835ba534a104c62aeb24569cb
nigusmach <nigusmachin@gmail.com>
2022-07-05 20:44:28 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,31 +1,13 @@
+#include "main.h"
 /**
- *
- *  *_islower - checks for a lowercase character
- *
- *   *@c: letter being tested
- *
- *    * Return: Always 0
- *
- *     */
-
-
-
+ *_islower - checks for a lowercase character
+ * @c: letter being tested
+ * Return: Always 0
+ */
 int _islower(int c)
-
 {
-
-
-
-		if (c >= 97 && c <= 122)
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
-
-Footer
-
-© 2022 GitHub, Inc.
