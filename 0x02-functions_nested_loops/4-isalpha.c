468c3a9783716bdb2c3f062961741a6951ff30a0
nigusmach <nigusmachin@gmail.com>
2022-07-05 20:46:43 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,13 @@
+#include "main.h"
 /**
-
  * _isalpha - check the code for Holberton School students.
- *
- *  *@c: letter being tested
- *
- *   * Return: Always 0
- *
- *    */
-
-
-
+ * @c: letter being tested
+ * Return: Always 0
+ */
  int _isalpha(int c)
-
 {
-
-		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
