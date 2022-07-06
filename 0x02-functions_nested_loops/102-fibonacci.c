8cd62066f09c30ed901dc629f71c962e09615ff5
nigusmach <nigusmachin@gmail.com>
2022-07-06 22:43:39 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 /**
  * main -prints multiple 3 and 5 numbers less than 1024
  * Return: always 0 (success)
@@ -11,7 +13,7 @@ int main(void)
 {
 
 	int n = 50;
-	long sum = 0;
+	uint64_t sum = 0;
 	int i;
 	int a = 0;
 	int b = 1;
@@ -21,7 +23,7 @@ int main(void)
 		sum = a + b;
 		a = b;
 		b = sum;
-		printf("%ld, ", sum);
+		printf("%"PRIu64"\n", sum);
 	}
 	return (0);
 }
