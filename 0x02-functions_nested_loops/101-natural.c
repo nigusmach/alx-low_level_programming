ef87569ed8b92e9c488f884899e9f02ba72da784
nigusmach <nigusmachin@gmail.com>
2022-07-06 14:11:12 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int n=1024;
-	int sum=0;
+	int n = 1024;
+	int sum = 0;
 	int i;
 	   
-	for(i=1;i<n;i++)
+	for(i = 1; i < n; i++)
 	{
-		if(i%3==0||i%5==0)
-			sum+=i;
+		if(i%3 == 0 || i%5 == 0)
+			sum+ = i;
 	}
-	printf("%d",sum);
+	printf("%d", sum);
 	printf("\n");
 	return 0;
 }
