272c5c0387097575de94e144da12c37bc7b74c3a
nigusmach <nigusmachin@gmail.com>
2022-07-06 21:46:44 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,53 +1,28 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
- *
- *  * main -prints multiple 3 and 5 numbers less than 1024
- *
- *   * Return: always 0 (success)
- *
- *    */
-
-
-
+ * main -prints multiple 3 and 5 numbers less than 1024
+ * Return: always 0 (success)
+ */
 int main(void)
-
-
-
 {
+	int n = 4000000;
+	int sum = 0;
+	int i;
+	int a= 0;
+	int b= 1;
+	int feb;
 
-
-
-		int n = 50;
-
-			long long int sum = 0;
-
-				int i;
-
-					int a= 0;
-
-						int b= 1;
-
-							
-
-							for (i = 1; i <= n; i++)
-
-									{
-
-												sum = a + b;
-
-														a=b;
-
-																b=sum;
-
-																		printf("%llu, ", sum);
-
-																			}
-
-								return (0);
-
+	for (i = 1; i <= n; i++)
+		feb=a+b;
+		a = b;
+		b = feb;
+		if (feb%2 == 0)
+		{
+			sum += feb;
+			printf("%llu, ", sum);
+			prinf("\n");
+		}
+	return (0);
 }
