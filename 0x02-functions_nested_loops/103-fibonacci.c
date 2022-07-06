ff8dafb2331c0aef8e1ccdcb123ee4d31d80dec1
nigusmach <nigusmachin.com>
2022-07-06 13:39:10 -0700

    commited

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main -prints multiple 3 and 5 numbers less than 1024
+ *
+ *   * Return: always 0 (success)
+ *
+ *    */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+		int n = 50;
+
+			long long int sum = 0;
+
+				int i;
+
+					int a= 0;
+
+						int b= 1;
+
+							
+
+							for (i = 1; i <= n; i++)
+
+									{
+
+												sum = a + b;
+
+														a=b;
+
+																b=sum;
+
+																		printf("%llu, ", sum);
+
+																			}
+
+								return (0);
+
+}
