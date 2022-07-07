9e5fcbf1376a3567db7f278172aa83bf20b81465
nigusmach <nigusmachin@gmail.com>
2022-07-07 15:32:30 +0100

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h";
+#include <stdio.h>
+/**
+ * is_uper -  checks for weather upper letter or not
+ * @c: third int
+ * Return: 1 if upper else 0
+ */
+ int _isupper(int c)
+ {
+ if (c >= 65 & c <=90)
+ return 1;
+ else
+ return 0;
+ }
