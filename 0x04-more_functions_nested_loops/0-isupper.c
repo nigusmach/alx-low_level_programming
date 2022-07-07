c36f3ec60fcab4017fc0df3a272d32cb7365886f
nigusmach <nigusmachin@gmail.com>
2022-07-07 15:47:46 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,12 @@
  * @c: the int for the paramaters of my function
  * Return: 0
  */
- int _isupper(int c)
- {
-	if (c >= 65 & c <=90)
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
 	{
-		return (1);
+	return (1);
 	}
 	else
-	{
-		return (0);
-	}
-	 return (0);
+	return (0);
 }
-
