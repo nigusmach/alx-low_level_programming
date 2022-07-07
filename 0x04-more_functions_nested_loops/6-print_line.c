c3211498ee95b56f785426f6ae4638032e12b3d0
nigusmach <nigusmachin@gmail.com>
2022-07-07 18:59:52 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,15 @@
  */
 void print_line(int n)
 {
-	int a = 0;
-
 	if (n > 0)
 	{
-		while (a <= n)
+		while (n != 0)
 		{
 			_putchar('_');
-			a++;
+			_putchar('\n');
+			n--;
 		}
-	 _putchar('\n');
-	}
+		}
 	else
 		_putchar('\n');
 }
