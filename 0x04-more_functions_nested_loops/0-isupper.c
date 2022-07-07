91b863b3d1cf25d18794ba688abb568fceb658a2
nigusmach <nigusmachin@gmail.com>
2022-07-07 20:48:45 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,14 @@
-/*
- * File: 0-isupper.c
- * Auth: Nigus D Machin
- */
 #include "main.h"
+
 /**
- * _isupper - Checks for uppercase characters.
- * @c: the int for the paramaters of my function.
- * Return: 1 if character is uppercase, 0 otherwise.
+ * _isupper - a function that checks for uppercase character
+ * @c: The int for the paramaters of my function
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	}
 	else
 		return (0);
 }
