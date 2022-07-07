e1ff8768921e0022794aa620a69eecd5f21033d2
nigusmach <nigusmachin@gmail.com>
2022-07-07 17:14:19 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,57 +1,28 @@
 #include "main.h"
-
 /**
- *
- *  * print_diagonal - function that draws a diagonal line on the terminal.
- *
- *   * @n: the int for the paramaters of my function
- *
- *    * Return: 0
- *
- *     */
-
-void print_diagonal(int n)
-
+ * print_square - function that checks for uppercase character.
+ * @size: the int for the paramaters of my function
+ * Return: 0
+ */
+void print_square(int size)
 {
+	int a = 0;
+	int b = 0;
 
-		int a = 0;
-
-			int b = 0;
-
-
-
-				if (n > 0)
-
-						{
-
-									while (a < n)
-
-												{
-
-																while (b < a)
-
-																				{
-
-																									_putchar(' ');
-
-																													b++;
-
-																																}
-
-																			a++;
-
-																						b = 0;
-
-																									_putchar('\\');
-
-																												_putchar('\n');
-
-																														}
-
-										}
-
-					else
-
-							_putchar('\n');
-
+	if (size > 0)
+	{
+		while (a < size)
+		{
+			while (b < size)
+			{
+				_putchar('#');
+				b++;
+			}
+		b = 0;
+		a++;
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
