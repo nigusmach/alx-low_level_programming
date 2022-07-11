d034aae34f2407555d5380e9ccd0f5fcb96d4d7f
nigusmach <nigusmachin.com>
2022-07-11 08:38:58 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *
+ *  * _strlen - check the code for Holberton School students.
+ *
+ *   * @s: declaration of *s and paramters for the function _strlen
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int _strlen(char *s)
+
+{
+
+		int c;
+
+
+
+			for (c = 0; s[c] != 0; c++)
+
+					{
+
+							}
+
+				return (c);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
