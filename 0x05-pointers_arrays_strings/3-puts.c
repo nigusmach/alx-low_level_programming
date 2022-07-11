88087ebf8c5ea6e2be2c6e97661fb30714c05c8b
nigusmach <nigusmachin@gmail.com>
2022-07-11 16:48:35 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,31 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * _puts - function that prints a string, followed by a new line
- *
- *   * @str: declaration of *str and paramters for the function _puts
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * _puts - function that prints a string, followed by a new line
+ * @str: declaration of *str and paramters for the function _puts
+ * Return: Always 0.
+ */
 void _puts(char *str)
-
 {
+	int c;
 
-		int c;
-
-
-
-			for (c = 0; str[c] != 0; c++)
-
-					{
-
-								_putchar(str[c]);
-
-									}
-
-				_putchar('\n');
-
+	for (c = 0; str[c] != 0; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
 }
