c99da670d096e7c089c69b794d2ff7ea49b356f3
nigusmach <nigusmachin@gmail.com>
2022-07-11 16:44:33 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,29 +1,15 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * swap_int - check the code for Holberton School students.
- *
- *   * @a: declaration of a and paramters for the function swap_int
- *
- *    * @b: declaration of b and paramters for the function swap_int
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * swap_int - that swaps the values of two integers.
+ * @a: declaration of a and paramters for the function swap_int
+ * @b: declaration of b and paramters for the function swap_int
+ * Return: Always 0.
+ */
 void swap_int(int *a, int *b)
-
 {
+	int c;
 
-		int c;
-
-
-
-			c = *a;
-
-				*a = *b;
-
-					*b = c;
-
+	c = *a;
+	*a = *b;
+	*b = c;
 }
