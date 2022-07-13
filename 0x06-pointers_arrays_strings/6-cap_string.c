0131aed2bf6568b69b1a3ee91f06c64c5f2a6e77
nigusmach <nigusmachin@gmail.com>
2022-07-13 21:32:23 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,37 @@
 #include "main.h"
-
-
-
 /**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
  *
- *  * reverse_array - Reverses the content of an array of integers.
- *
- *   * @a: The array of integers to be reversed.
- *
- *    * @n: The number of elements in the array.
- *
- *     */
-
-void reverse_array(int *a, int n)
-
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
 {
+	int index = 0;
 
-		int tmp, index;
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
+		index++;
+	}
 
-			for (index = n - 1; index >= n / 2; index--)
-
-					{
-
-								tmp = a[n - 1 - index];
-
-										a[n - 1 - index] = a[index];
-
-												a[index] = tmp;
-
-													}
-
+	return (str);
 }
