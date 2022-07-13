a74c34a19b8593c8e9eee75321f92044b8f9bcd3
nigusmach <nigusmachin@gmail.com>
2022-07-13 23:03:24 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
 	putchar('\n');
 	return (0);
 }
