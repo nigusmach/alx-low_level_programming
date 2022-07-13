7feb693b6b0aa309ed91399023abdbb994e36b43
nigusmach <nigusmachin@gmail.com>
2022-07-13 21:44:39 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
- * _strcat -  function that concatenates two strings.
+ * *_strncat -  function that concatenates two strings.
  * @src: parameter to a src
  * @dest: parameter to append
  * Return: Always 0.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
+	int i = 0;
+	int j = 0;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0' && b < n)
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
-		dest[a] = src[b];
-		b++;
-		a++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
 	return (dest);
 }
