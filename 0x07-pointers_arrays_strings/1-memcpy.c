0779ffb7014a8b73f7dde3f7612a936f7518170d
nigusmach <nigusmachin@gmail.com>
2022-07-18 20:12:18 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
-
+	
 	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+		dest[index] = src[index];
 
 	return (dest);
 }
