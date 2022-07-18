2f198ffdcb433c248afd17937e0fff96fd264ecb
nigusmach <nigusmachin@gmail.com>
2022-07-18 20:18:15 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,6 @@
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
- *
  * Return: A pointer to the destination buffer @dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
