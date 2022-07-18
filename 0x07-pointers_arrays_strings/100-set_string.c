8c618083201654bb2f3c50697e9f855b5a04f272
nigusmach <nigusmachin.com>
2022-07-18 12:46:51 -0700

    my first commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+
+
+/**
+ *
+ *  * set_string - Sets the value of a pointer to a char.
+ *
+ *   * @s: The pointer.
+ *
+ *    * @to: The char.
+ *
+ *     */
+
+void set_string(char **s, char *to)
+
+{
+
+		*s = to;
+
+}
