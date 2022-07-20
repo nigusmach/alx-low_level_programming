3aa5526f125c5d161a13c2fbf529d5404253d25b
nigusmach <nigusmachin.com>
2022-07-20 01:51:36 -0700

    my first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,33 @@
-#include "main.h"
+#include "holberton.h"
+
+
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
- */
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+ *
+ *  * _puts_recursion - Prints a string, followed by a new line.
+ *
+ *   * @s: The string to be printed.
+ *
+ *    */
 
-	else
-		_putchar('\n');
+void _puts_recursion(char *s)
+
+{
+
+		if (*s)
+
+				{
+
+							_putchar(*s);
+
+									_puts_recursion(s + 1);
+
+										}
+
+
+
+			else
+
+						_putchar('\n');
+
 }
