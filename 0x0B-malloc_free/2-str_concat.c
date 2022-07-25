b13b64ed45b548012bf8b554936c3cfc1d63a8ea
nigusmach <nigusmachin@gmail.com>
2022-07-25 23:05:29 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,78 +1,39 @@
-i#include "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
 
-
-
 /**
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
  *
- *  * str_concat - Concatenates two strings.
- *
- *   * @s1: The string to be concatenated upon.
- *
- *    * @s2: The string to be concatenated to s1.
- *
- *     *
- *
- *      * Return: If concatenation fails - NULL.
- *
- *       *         Otherwise - a pointer the newly-allocated space in memory
- *
- *        *                     containing the concatenated strings.
- *
- *         */
-
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
+ */
 char *str_concat(char *s1, char *s2)
-
 {
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-		char *concat_str;
+	if (s1 == NULL)
+		s1 = "";
 
-			int index, concat_index = 0, len = 0;
+	if (s2 == NULL)
+		s2 = "";
 
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
+	concat_str = malloc(sizeof(char) * len);
 
-				if (s1 == NULL)
+	if (concat_str == NULL)
+		return (NULL);
 
-							s1 = "";
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
-
-					if (s2 == NULL)
-
-								s2 = "";
-
-
-
-						for (index = 0; s1[index] || s2[index]; index++)
-
-									len++;
-
-
-
-							concat_str = malloc(sizeof(char) * len);
-
-
-
-								if (concat_str == NULL)
-
-											return (NULL);
-
-
-
-									for (index = 0; s1[index]; index++)
-
-												concat_str[concat_index++] = s1[index];
-
-
-
-										for (index = 0; s2[index]; index++)
-
-													concat_str[concat_index++] = s2[index];
-
-
-
-											return (concat_str);
-
-}1:wq!
-
+	return (concat_str);
+}
