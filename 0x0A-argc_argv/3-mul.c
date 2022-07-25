c665d44ffdf81696624056d293b6197a9c30d99e
nigusmach <nigusmachin@gmail.com>
2022-07-25 22:35:31 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,77 +1,29 @@
-/*
- *
- *  * File: 4-add.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- *  * main - Prints the addition of positive numbers,
- *
- *   *        followed by a new line.
- *
- *    * @argc: The number of arguments passed to the program.
- *
- *     * @argv: An array of pointers to the arguments.
- *
- *      *
- *
- *       * Return: If one of the numbers contains symbols that are non-digits - 1.
- *
- *        *         Otherwise - 0.
- *
- *         */
-
+ * Return: If the program receives two arguments - 0.
+ *         If the program does not receive two arguments - 1.
+ */
 int main(int argc, char *argv[])
-
 {
+	int num1, num2, prod;
 
-		int num, digit, sum = 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
+	printf("%d\n", prod);
 
-			for (num = 1; num < argc; num++)
-
-					{
-
-								for (digit = 0; argv[num][digit]; digit++)
-
-											{
-
-															if (argv[num][digit] < '0' || argv[num][digit] > '9')
-
-																			{
-
-																								printf("Error\n");
-
-																												return (1);
-
-																															}
-
-																	}
-
-
-
-										sum += atoi(argv[num]);
-
-											}
-
-
-
-				printf("%d\n", sum);
-
-
-
-					return (0);
-
+	return (0);
 }
