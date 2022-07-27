9929c22b6f2c99940b60b5bf4f35f94a9c4d8386
nigusmach <nigusmachin@gmail.com>
2022-07-27 20:44:53 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,2 +1,18 @@
-#include "main.h";
+#include "main.h"
+/**
+ * _memset - Fills the first n bytes of the memory area
+ *          pointed to by @s with the constant byte @c.
+ * @s: parameter for the array
+ * @b: parameter for the values in the array
+ * @n: parameter for the number of times to print the new values
+ * Return: Always 0.
+ */
 char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+		s[index] = b;
+
+	return (s);
+}
