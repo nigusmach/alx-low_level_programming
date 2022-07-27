397b8a115ac625a0626b46888555202762329bb8
nigusmach <nigusmachin@gmail.com>
2022-07-27 20:41:35 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,59 +1,30 @@
 /*
- *
- *  * File: 1-strncat.c
- *
- *   * Auth: Nigus Machin
- *
- *    */
-
-
+ * File: 1-strncat.c
+ * Auth: Nigus Machin
+ */
 
 #include "main.h"
 
-
-
 /**
- *
- *  * _strncat - Concatenates two strings using at most
- *
- *   *            an inputted number of bytes from src.
- *
- *    * @dest: The string to be appended upon.
- *
- *     * @src: The string to be appended to dest.
- *
- *      * @n: The number of bytes from src to be appended to dest.
- *
- *       * Return: A pointer to the resulting string dest.
- *
- *        */
-
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strncat(char *dest, char *src, int n)
-
 {
+	int i = 0;
+	int j = 0;
 
-		int i = 0;
-
-			int j = 0;
-
-
-
-				while (dest[i] != '\0')
-
-							i++;
-
-					while (src[j] != '\0' && j < n)
-
-							{
-
-										dest[i] = src[j];
-
-												j++;
-
-														i++;
-
-															}
-
-						return (dest);
-
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
 }
