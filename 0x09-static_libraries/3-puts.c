6b90a5d083adae0b852b1ae1281c320a179a9b32
nigusmach <nigusmachin@gmail.com>
2022-07-27 18:51:13 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,2 +1,16 @@
-void _puts(char *s){
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: declaration of *str and paramters for the function _puts
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
