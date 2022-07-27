ffcdc7ef53139e8d43467669465a9fa863324667
nigusmach <nigusmachin@gmail.com>
2022-07-27 21:51:45 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,18 @@
 #include "main.h"
-int _putchar(char c)
+/**
+ * main - Prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char ch = c;
-
- _putchar(ch);  
-  return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-
