2ca8c95d008ff5b2879fb29116d26d4f3e2f9ec8
nigusmach <nigusmachin@gmail.com>
2022-08-01 23:23:47 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,3 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-/*
- * File: 3-function_like_macro.h
- * Auth: Nigus Machin
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef ABS
+#define ABS(x) ((x) < 0 ? (x * -1) : (x))
 #endif
