fcde433eeae7d3eb769940e90572f725c6970e74
nigusmach <nigusmachin@gmail.com>
2022-08-01 22:47:40 +0100

    Update dog.c

--- 0x0E-structures_typedef/dog.c
+++ 0x0E-structures_typedef/dog.c
@@ -1,65 +1,14 @@
-#ifndef DOG_H
-
-#define DOG_H
-
-
-
-/*
- *
- *  * File: dog.h
- *
- *   * Auth: Brennan D Baraban
- *
- *    * Desc: Header file that defines a new type struct dog.
- *
- *     */
-
-
-
+#include "dog.h"
+#include <stdlib.h>
 /**
- *
- *  * struct dog - A new type describing a dog.
- *
- *   * @name: The name of the dog.
- *
- *    * @age: The age of the dog.
- *
- *     * @owner: The owner of the dog.
- *
- *      */
-
-struct dog
-
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
 {
-
-		char *name;
-
-			float age;
-
-				char *owner;
-
-};
-
-
-
-/**
- *
- *  * dog_t - Typedef for struct dog
- *
- *   */
-
-typedef struct dog dog_t;
-
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-
-
-#endif
+	char *name;
+	float age;
+	char *owner;
+}:
