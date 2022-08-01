77af12b5f1551d31668ee4de41a702af844eef5c
nigusmach <nigusmachin@gmail.com>
2022-08-01 22:59:41 +0100

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,25 +1,3 @@
-#ifndef OBJECT_LIKE_MACRO_H
-
-#define OBJECT_LIKE_MACRO_H
-
-
-
-/*
- *
- *  * File: 0-object_like_macro.h
- *
- *   * Auth: Brennan D Baraban
- *
- *    * Desc: Header file that defines a macro named
- *
- *     *       SIZE as an abbreviation for the token 1024.
- *
- *      */
-
-
-
+#ifndef SIZE
 #define SIZE 1024
-
-
-
-#endif
+#endif 
