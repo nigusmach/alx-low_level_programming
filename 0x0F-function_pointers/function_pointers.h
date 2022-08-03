b32aacaede489ee13eed472347c9a63cc561ad68
nigusmach <nigusmachin@gmail.com>
2022-08-03 16:53:01 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,17 @@
-int _putchar(char c);
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+
+#endif
