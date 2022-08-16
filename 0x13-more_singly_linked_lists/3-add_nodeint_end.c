d2c2d2391cce49db142a262e50dbb123a02d56fa
nigusmach <nigusmachin@gmail.com>
2022-08-16 16:59:44 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,43 +1,36 @@
 #include "lists.h"
 
-#include <stdio.h>
-
-
-
 /**
+ * add_nodeint_end - Adds a new node at the
+ *                   end of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
  *
- *  * print_listint - Prints all the elements of a listint_t list.
- *
- *   * @h: A pointer to the head of the list_t list.
- *
- *    *
- *
- *     * Return: The number of nodes in the list_t list.
- *
- *      */
-
-size_t print_listint(const listint_t *h)
-
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new, *last;
 
-		size_t nodes = 0;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
+	new->n = n;
+	new->next = NULL;
 
+	if (*head == NULL)
+		*head = new;
 
-			while (h)
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
 
-					{
-
-								nodes++;
-
-										printf("%d\n", h->n);
-
-												h = h->next;
-
-													}
-
-
-
-				return (nodes);
-
+	return (*head);
 }
