345399ad0afe232063a638874622ef8936f2e359
nigusmach <nigusmachin@gmail.com>
2022-08-18 20:01:59 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,37 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
  *
- *  * set_bit - Sets the value of a bit at a given index to 1.
- *
- *   * @n: A pointer to the bit.
- *
- *    * @index: The index to set the value at - indices start at 0.
- *
- *     *
- *
- *      * Return: If an error occurs - -1.
- *
- *       *         Otherwise - 1.
- *
- *        */
-
-int set_bit(unsigned long int *n, unsigned int index)
-
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int xor = n ^ m, bits = 0;
 
-		if (index >= (sizeof(unsigned long int) * 8))
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
 
-					return (-1);
-
-
-
-			*n ^= (1 << index);
-
-
-
-				return (1);
-
+	return (bits);
 }
