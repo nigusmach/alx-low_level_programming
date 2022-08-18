98da4166b3ac9c5379305db91a56c95be5568294
nigusmach <nigusmachin@gmail.com>
2022-08-18 19:59:14 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,25 +1,19 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * set_bit - Sets the value of a bit at a given index to 1.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
  *
- *  * print_binary - Prints the binary representation of a number.
- *
- *   * @n: The number to be printed in binary.
- *
- *    */
-
-void print_binary(unsigned long int n)
-
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-		if (n > 1)
+	*n ^= (1 << index);
 
-					print_binary(n >> 1);
-
-
-
-			_putchar((n & 1) + '0');
-
+	return (1);
 }
