fbe4fab25eaf85477263323acc1f96590153a44e
nigusmach <nigusmachin@gmail.com>
2022-10-21 19:00:45 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,35 +1,24 @@
+/*
+ * File: 1-djb2.c
+ * Auth: Nigus Machin
+ */
+
+#include "hash_tables.h"
+
 /**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
  *
- *  * hash_djb2 - implementation of the djb2 algorithm
- *
- *   * @str: string used to generate hash value
- *
- *    *
- *
- *     * Return: hash value
- *
- *      */
-
+ * Return: The calculated hash.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
-
 {
+	unsigned long int hash;
+	int c;
 
-		unsigned long int hash;
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-			int c;
-
-
-
-				hash = 5381;
-
-					while ((c = *str++))
-
-							{
-
-										hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-											}
-
-						return (hash);
-
+	return (hash);
 }
