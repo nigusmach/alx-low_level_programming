59e422006c656838d825b65be4500af712f55271
nigusmach <nigusmachin@gmail.com>
2022-10-21 18:56:05 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,63 +1,31 @@
 /*
- *
- *  * File: 0-hash_table_create.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+ * File: 0-hash_table_create.c
+  */
 
 #include "hash_tables.h"
 
-
-
 /**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- *  * hash_table_create - Creates a hash table.
- *
- *   * @size: The size of the array.
- *
- *    *
- *
- *     * Return: If an error occurs - NULL.
- *
- *      *         Otherwise - a pointer to the new hash table.
- *
- *       */
-
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
-
 {
+	hash_table_t *ht;
+	unsigned long int i;
 
-		hash_table_t *ht;
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-			unsigned long int i;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-
-
-				ht = malloc(sizeof(hash_table_t));
-
-					if (ht == NULL)
-
-								return (NULL);
-
-
-
-						ht->size = size;
-
-							ht->array = malloc(sizeof(hash_node_t *) * size);
-
-								if (ht->array == NULL)
-
-											return (NULL);
-
-									for (i = 0; i < size; i++)
-
-												ht->array[i] = NULL;
-
-
-
-										return (ht);
-
+	return (ht);
 }
