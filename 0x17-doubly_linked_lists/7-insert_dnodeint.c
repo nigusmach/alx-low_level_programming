640be37418823836a5e88ab21cc87edfe1f45d56
nigusmach <nigusmachin@gmail.com>
2022-10-27 11:31:04 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,49 +1,46 @@
 /*
- *
- *  * File: 6-sum_dlistint.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+ * File: 7-insert_dnodeint.c
+ * Auth: Nigus Machin
+ */
 
 #include "lists.h"
 
-
-
 /**
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- *  * sum_dlistint - Sums all the data of a dlistint_t list.
- *
- *   * @head: The head of the dlistint_t list.
- *
- *    *
- *
- *     * Return: The sum of all the data.
- *
- *      */
-
-int sum_dlistint(dlistint_t *head)
-
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp = *h, *new;
 
-		int sum = 0;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-			while (head)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-					{
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-								sum += head->n;
-
-										head = head->next;
-
-											}
-
-
-
-				return (sum);
-
+	return (new);
 }
