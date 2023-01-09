ccd3a2e97f97564a008d65757fe88ad980a7be30
nigusmach <nigusmachin@gmail.com>
2023-01-09 17:50:50 +0000

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * _isdigit - function that checks for uppercase character.
- * @c: the int for the paramaters of my function
- * Return: 0
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
  */
+
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-	return (1);
-	}
-	else
-	return (0);
+
+	return (c >= 48 && c <= 57);
 }
