6778199b11598f0dcdf62d4c00390cf3fa12fb39
nigusmach <nigusmachin@gmail.com>
2023-01-09 17:56:05 +0000

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,13 +1,18 @@
 #include "main.h"
+
 /**
- *_islower - checks for a lowercase character
- * @c: letter being tested
- * Return: Always 0
+ * _islower - check the code for Holberton School students.
+ *
+ * @c:  is a parameter
+ *
+ * Return: Always 0.
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
