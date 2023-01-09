57abfe311d4b1e9ca31bacf2052e160ecc37df45
nigusmach <nigusmachin@gmail.com>
2023-01-09 17:48:02 +0000

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,20 +1,16 @@
-/*
- * File: 0-isupper.c
- * Auth: Nigus Machin
- */
-
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- * Return: 1 if character is uppercase, 0 otherwise.
+ * _isupper - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
  */
+
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
 
-	else
-		return (0);
+	return (c >= 65 && c <= 90);
 }
